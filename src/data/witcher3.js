385eac02f10c19acee17371d7dc09d87ee58f813
Paweł Romik <leoneczek15@gmail.com>
2023-08-04 15:07:32 +0200

    Added Quest Tracker

---
+++ src/data/witcher3.js
@@ -0,0 +1,124 @@
+const quests = [
+{
+    id: 0, 
+    name: 'Lilac and Gooseberries',
+    location: 'White Orchard',
+    type: 'Main Quest',
+    level: '0',
+    desc: "It's the second main quest and the first following the tutorial quest"
+},
+{
+    id: 1, 
+    name: 'The Beast of White Orchard',
+    location: 'White Orchard',
+    type: 'Main Quest',
+    level: '3',
+    desc: "It's the third main quest of The Witcher 3: Wild Hunt. It acts as a sub-quest to Lilac and Gooseberries as it must be completed before the player can continue that quest."
+},
+{
+    id: 2, 
+    name: 'The Incident at White Orchard',
+    location: 'White Orchard',
+    type: 'Main Quest',
+    level: '2',
+    desc: "It's a minor main quest that occurs at the conclusion of the quest Lilac and Gooseberries"
+},
+{
+    id: 3, 
+    name: 'A Frying Pan, Spick and Span',
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '0',
+    desc: ''
+},
+{
+    id: 4, 
+    name: 'Faithful Friend',
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '30',
+    desc: 'This quest is only accessible during the Empress ending and it must be completed before Something Ends, Something Begins.'
+},
+{
+    id: 5, 
+    name: 'Missing in Action',
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '0'
+},
+{
+    id: 6, 
+    name: "On Death's Bed",
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '2',
+    desc: "On Death's Bed is a secondary quest in The Witcher 3: Wild Hunt, given by the herbalist Tomira in White Orchard. "
+},
+{
+    id: 7, 
+    name: 'Precious Cargo',
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '0',
+    desc: ''
+},
+{
+    id: 8, 
+    name: 'Twisted Firestarter',
+    location: 'White Orchard',
+    type: 'Secondary Quest',
+    level: '0',
+    desc: ''
+},
+{
+    id: 9, 
+    name: 'Contract: Devil by the Well',
+    location: 'White Orchard',
+    type: 'Contract',
+    level: '2',
+    desc: ''
+},
+{
+    id: 10, 
+    name: 'Deserter Gold',
+    location: 'White Orchard',
+    type: 'Treasure Hunt',
+    level: '3',
+    desc: ''
+},
+{
+    id: 11, 
+    name: 'Dirty Funds',
+    location: 'White Orchard',
+    type: 'Treasure Hunt',
+    level: '2',
+    desc: ''
+},
+{
+    id: 12, 
+    name: 'Scavenger Hunt: Viper School Gear',
+    location: 'White Orchard',
+    type: 'Treasure Hunt',
+    level: '6',
+    desc: ''
+},
+{
+    id: 13, 
+    name: 'Temerian Valuables',
+    location: 'White Orchard',
+    type: 'Treasure Hunt',
+    level: '4',
+    desc: ''
+},
+{
+    id: 14, 
+    name: 'Temerian Valuables',
+    location: 'Skellige',
+    type: 'Treasure Hunt',
+    level: '4',
+    desc: ''
+},
+
+];
+
+export default quests;
--- src/pages/Wildhunt.tsx
+++ src/pages/Wildhunt.tsx
@@ -1,13 +1,177 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/style.scss';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import quests from '../data/witcher3';
+
+interface Quest {
+	id: number;
+	name: string;
+	location: string;
+	type: string;
+	level: string;
+}
 
 function Wildhunt() {
+	const [completedQuests, setCompletedQuests] = useState<number[]>([]);
+	const [mapImg, changeMap] = useState<number>(0);
+	const locationGroup: { [location: string]: { [type: string]: Quest[] } } = {};
+
+	useEffect(() => {
+		const CompletedQuestsTemp = localStorage.getItem('witcher3Quests');
+		if (CompletedQuestsTemp) {
+			setCompletedQuests(JSON.parse(CompletedQuestsTemp));
+		}
+	}, []);
+
+	quests.forEach((quest) => {
+		if (!locationGroup[quest.location]) {
+			locationGroup[quest.location] = {};
+		}
+		if (!locationGroup[quest.location][quest.type]) {
+			locationGroup[quest.location][quest.type] = [];
+		}
+		locationGroup[quest.location][quest.type].push(quest);
+	});
+
+	const completeQuest = (id: number) => {
+		setCompletedQuests((prevCompletedQuests) => {
+			const updatedQuests = prevCompletedQuests.includes(id)
+				? prevCompletedQuests.filter((previd) => previd !== id)
+				: [...prevCompletedQuests, id];
+
+			localStorage.setItem('witcher3Quests', JSON.stringify(updatedQuests));
+			return updatedQuests;
+		});
+	};
+
+	const changeCompletion = (type: Quest[]) => {
+		const totalQuests = type.length;
+		const completedQuestsCount = type.filter((quest) =>
+			completedQuests.includes(quest.id)
+		).length;
+
+		return `${completedQuestsCount + '/' + totalQuests}`;
+	};
+
+	const showMap = () => {
+		if (mapImg !== 0) {
+			const i = mapImg - 1;
+			let img;
+			try {
+				img = require('../assets/witcher3/map/' + i + '.png');
+				return (
+					<div className='map' onClick={() => changeMap(0)}>
+						<div
+							className='mapOn'
+							style={{ backgroundImage: `url(` + img + `)` }}
+						>
+							<button onClick={() => changeMap(0)}>X</button>
+						</div>
+					</div>
+				);
+			} catch (error) {
+				return (
+					<div className='map' onClick={() => changeMap(0)}>
+						<div>
+							<button onClick={() => changeMap(0)}>X</button>
+							<p>{quests[i].desc}</p>
+						</div>
+					</div>
+				);
+			}
+		} else return <></>;
+	};
+
+	const sortType = (location: { [type: string]: Quest[] }) => {
+		return Object.entries(location).map(([type, typeQuest]) => {
+			const completed = typeQuest.filter((quest) =>
+				completedQuests.includes(quest.id)
+			);
+			const incompleted = typeQuest.filter(
+				(quest) => !completedQuests.includes(quest.id)
+			);
+
+			return (
+				<details key={type}>
+					<summary>
+						{type} - {changeCompletion(typeQuest)}
+					</summary>
+					{incompleted
+						.sort((a, b) => Number(a.level) - Number(b.level))
+						.map((item) => (
+							<div
+								className={`quest ${
+									completedQuests.includes(item.id) ? 'completed' : ''
+								}`}
+								key={item.id}
+								onClick={() => changeMap(item.id + 1)}
+							>
+								<div className='questinfo'>
+									<span>{item.name}</span>
+									<span>{item.type}</span>
+								</div>
+								<div className='questEvent'>
+									<span>{item.level !== '0' ? item.level + ' lvl' : '-'}</span>
+									<button
+										onClick={(e) => {
+											e.stopPropagation();
+											completeQuest(item.id);
+										}}
+									>
+										{completedQuests.includes(item.id) ? 'X' : ''}
+									</button>
+								</div>
+							</div>
+						))}
+					{completed
+						.sort((a, b) => Number(a.level) - Number(b.level))
+						.map((item) => (
+							<div
+								className={`quest ${
+									completedQuests.includes(item.id) ? 'completed' : ''
+								}`}
+								key={item.id}
+								onClick={() => changeMap(item.id + 1)}
+							>
+								<div className='questinfo'>
+									<span>{item.name}</span>
+									<span>{item.type}</span>
+								</div>
+								<div className='questEvent'>
+									<span>{item.level !== '0' ? item.level + ' lvl' : '-'}</span>
+									<button
+										onClick={(e) => {
+											e.stopPropagation();
+											completeQuest(item.id);
+										}}
+									>
+										{completedQuests.includes(item.id) ? 'X' : ''}
+									</button>
+								</div>
+							</div>
+						))}
+				</details>
+			);
+		});
+	};
+
+	const sortLocation = () => {
+		return Object.entries(locationGroup).map(([location, questLocation]) => (
+			<details className='locationwitcher' key={location}>
+				<summary>{location}</summary>
+				{sortType(questLocation)}
+			</details>
+		));
+	};
+
 	return (
 		<>
 			<Header counter={0} onButtonClick={''} />
-			<main className='quests'></main>
+			<main className='quests'>
+				{showMap()}
+				{sortLocation()}
+			</main>
 			<Footer counter={0} />
 		</>
 	);
