b1f4990044a02e67c8d616268d2588f35e18fbcf
Paweł Romik <leoneczek15@gmail.com>
2023-09-15 17:59:50 +0200

    Added Witcher 1 Prologue quests

--- src/data/witcher1.ts
+++ src/data/witcher1.ts
@@ -2,11 +2,40 @@
 const actOne = [
     {
         id: 0, 
-        name: 'Lilac and Gooseberries',
-        location: 'Act 1',
+        name: 'Defending Kaer Morhen',
+        location: 'Prologue',
         type: 'Main Quest',
         level: '0',
         desc: "It's the first main quest"
+    },
+    {
+        id: 1, 
+        name: 'Witchers Secrets',
+        location: 'Prologue',
+        type: 'Main Quest',
+        level: '0',
+        desc: "After Defending Kaer Morhen"
+    },{
+        id: 2, 
+        name: 'A Potion for Triss',
+        location: 'Prologue',
+        type: 'Secondary Quest',
+        level: '0',
+        desc: "After Defending Kaer Morhen"
+    },{
+        id: 3, 
+        name: "Berengar's Secret",
+        location: 'Prologue',
+        type: 'Secondary Quest',
+        level: '0',
+        desc: "After Defending Kaer Morhen"
+    },{
+        id: 4, 
+        name: "Fistfight (Eskel)",
+        location: 'Prologue',
+        type: 'Secondary Quest',
+        level: '0',
+        desc: "After Defending Kaer Morhen"
     },
 ]
 
