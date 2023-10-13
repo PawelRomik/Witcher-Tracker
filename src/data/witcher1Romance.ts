ab1132bcaa03765aec42538799b7af6de468457d
Paweł Romik <leoneczek15@gmail.com>
2023-10-13 16:56:17 +0200

    Added act 2

--- src/data/witcher1Romance.ts
+++ src/data/witcher1Romance.ts
@@ -22,7 +22,7 @@ const actOne = [
 		id: 2,
 		name: "Vesna",
 		location: "Act One",
-		type: "Complete quest 'She's No Early Bird' ",
+		type: "Complete quest 'She's No Early Bird'",
 		level: "0",
 		desc: "",
 	},
@@ -36,6 +36,49 @@ const actOne = [
 	},
 ];
 
-const quests = [...prologue, ...actOne];
+const actTwo = [
+	{
+		id: 4,
+		name: "Half-elf",
+		location: "Act Two",
+		type: "Complete quest 'A Ghost Story'",
+		level: "0",
+		desc: "",
+	},
+	{
+		id: 5,
+		name: "Prostitutes",
+		location: "Act Two",
+		type: "All give the same card. Can be 'bought' with flowers after the Working Girls Side-Quest, or before with money.",
+		level: "0",
+		desc: "",
+	},
+	{
+		id: 6,
+		name: "Gossip",
+		location: "Act Two",
+		type: "She walks around and wants a pair of gloves or a diamond.",
+		level: "0",
+		desc: "",
+	},
+	{
+		id: 7,
+		name: "Shani",
+		location: "Act Two",
+		type: "After the party where you invite a friend, ask about the battle and make sure you have a red rose to offer her.",
+		level: "0",
+		desc: "",
+	},
+	{
+		id: 8,
+		name: "Morenn",
+		location: "Act Two",
+		type: "Get the side-quest A Lost Lamb from Vaska. Bring her a wolf pelt and select the middle option.",
+		level: "0",
+		desc: "",
+	},
+];
+
+const quests = [...prologue, ...actOne, ...actTwo];
 
 export default quests;
